e45b1443210dccade735e1e76ccdc2f3ac066248
lanbots <linbooc@outlook.com>
2016-03-11 13:34:06 +0800

    UI设计02（登录界面设计）--（其中Test为UI02）

---
+++ UI03/src/com/lanbots/androiddemo/WelcomActivity.java
@@ -0,0 +1,18 @@
+package com.lanbots.androiddemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+	
+	
+
+}
